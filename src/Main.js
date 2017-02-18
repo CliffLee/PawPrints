034c4f03035e99ee86ca4f6b5c772ebf7e32661d
MiLeung <michaeleleung@gmail.com>
2017-02-17 23:53:17 -0800

    navigator boilerplate

---
+++ src/Main.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  Navigator
+} from 'react-native';
+
+import Login from './components/Login';
+import Map from './components/Map';
+
+const ROUTES = {
+  Login,
+  Map
+};
+
+export default class Main extends React.Component {
+  render() {
+    return (
+      <Navigator
+        initialRoute={{ title: 'Login' }}
+        renderScene={route => {
+          let Component = ROUTES[route.title];
+          return (<Component />);
+        }}
+      />
+    );
+  }
+}
